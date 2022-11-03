0c0d05715cb4a5f17bc6d9302e891efc506c0c7d
joemardev <joemarb.dev@gmail.com>
2022-11-03 13:38:44 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            main : '#080b13',
+            semiMain : '#1a1f2d',
+            primary : '#0671f4',
+            secondary : '#c9c9c9',
+            link : '#8f8f8f'
+        },
+      }
+    }
+  }
